6d01427ccfc9f804ae1c05e68b557126c212f451
rad1k4l <zeynalshamkhal@gmail.com>
2023-11-12 19:20:48 +0400

    add root path to sys

--- src/main.py
+++ src/main.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+import sys
+path_root = Path(__file__).parents[2]
+sys.path.append(str(path_root))
+
 from src.grabber.grabber_service import get_content, get_parsed_page
 from src.parser.parse_service import parse_make_urls
 from src.config.general import TURBO_AZ
